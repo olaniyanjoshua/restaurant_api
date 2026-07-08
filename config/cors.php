99957275cdef23c042d27fc04eb4867c169772b4
joshua olaniyan <olaniyanjoshua@gmail.com>
2026-07-08 04:31:55 -0700

    Fixed the allowed origins

--- config/cors.php
+++ config/cors.php
@@ -10,7 +10,7 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:3000',
-        'https://restaurant-api-hudb.onrender.com'
+        'https://react-restaurant-chi.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
